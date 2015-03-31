90828c8939a15ff1a8213f7df4feff5df77ce346
Jeppe Toustrup <jeppe@tenzer.dk>
2015-03-31 15:27:09 +0200

    Add option to specify the target commit or branch when creating a release

--- cmd.go
+++ cmd.go
@@ -246,6 +246,7 @@ func releasecmd(opt Options) error {
 	tag := cmdopt.Tag
 	name := nvls(cmdopt.Name, tag)
 	desc := nvls(cmdopt.Desc, tag)
+	target := nvls(cmdopt.Target)
 	draft := cmdopt.Draft
 	prerelease := cmdopt.Prerelease
 
@@ -256,11 +257,12 @@ func releasecmd(opt Options) error {
 	}
 
 	params := ReleaseCreate{
-		TagName:    tag,
-		Name:       name,
-		Body:       desc,
-		Draft:      draft,
-		Prerelease: prerelease,
+		TagName:         tag,
+		TargetCommitish: target,
+		Name:            name,
+		Body:            desc,
+		Draft:           draft,
+		Prerelease:      prerelease,
 	}
 
 	/* encode params as json */
