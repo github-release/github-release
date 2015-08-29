34e99581b40f60a42947b925d7761b49200f0504
Nicolas Hillegeer <nicolas@hillegeer.com>
2015-08-29 23:49:14 +0200

    api: support uploading from stdin/pipes

--- api.go
+++ api.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -14,34 +16,61 @@ const (
 	GH_URL  = "https://github.com"
 )
 
+// materializeFile takes a physical file or stream (named pipe, user input,
+// ...) and returns an io.Reader and the number of bytes that can be read
+// from it.
+func materializeFile(f *os.File) (io.Reader, int64, error) {
+	fi, err := f.Stat()
+	if err != nil {
+		return nil, 0, err
+	}
+
+	// If the file is actually a char device (like user typed input)
+	// or a named pipe (like a streamed in file), buffer it up.
+	//
+	// When uploading a file, you need to either explicitly set the
+	// Content-Length header or send a chunked request. Since the
+	// github upload server doesn't accept chunked encoding, we have
+	// to set the size of the file manually. Since a stream doesn't have a
+	// predefined length, it's read entirely into a byte buffer.
+	if fi.Mode()&(os.ModeCharDevice|os.ModeNamedPipe) == 1 {
+		vprintln("input was a stream, buffering up")
+
+		var buf bytes.Buffer
+		n, err := buf.ReadFrom(f)
+		if err != nil {
+			return nil, 0, errors.New("req: could not buffer up input stream: " + err.Error())
+		}
+		return &buf, n, err
+	}
+
+	// We know the os.File is most likely an actual file now.
+	n, err := GetFileSize(f)
+	return f, n, err
+}
+
 /* create a new request that sends the auth token */
 func NewAuthRequest(method, url, bodyType, token string, headers map[string]string, body io.Reader) (*http.Request, error) {
 	vprintln("creating request:", method, url, bodyType, token)
+
+	var n int64 // content length
+	var err error
+	if f, ok := body.(*os.File); ok {
+		// Retrieve the content-length and buffer up if necessary.
+		body, n, err = materializeFile(f)
+		if err != nil {
+			return nil, err
+		}
+	}
 
 	req, err := http.NewRequest(method, url, body)
 	if err != nil {
 		return nil, err
 	}
 
-	if body != nil {
-		switch v := body.(type) {
-		case *os.File:
-			/* when uploading a file, you need to either excplicitly set the
-			 * Content-Length header or send a chunked request. Since the
-			 * github upload server doesn't accept chunked encoding, we have
-			 * to set the size of the file manually. If the file is actually
-			 * stdin, this won't work, and we'll just quit for now.
-			 *
-			 * TODO: if stdin, read everything into a byte buffer, take the
-			 * length and send it. */
-			off, err := GetFileSize(v)
-			if err != nil {
-				return nil, err
-			}
-
-			req.ContentLength = off
-			vprintln("setting content-length to", off)
-		}
+	if n != 0 {
+		vprintln("setting content-length to", n)
+		req.ContentLength = n
 	}
 
 	if bodyType != "" {
